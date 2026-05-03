af84d9ebd652c55fda9362c92ed8683b2751078f
VictorMartosSegura <victormartossegura@elpuig.xeill.net>
2026-05-04 01:55:28 +0200

    Add Spring Boot application entry point

---
+++ src/main/java/com/autovalor/AutovalorApplication.java
@@ -0,0 +1,12 @@
+package com.autovalor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutovalorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutovalorApplication.class, args);
+    }
+}
