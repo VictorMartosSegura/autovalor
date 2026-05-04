ddfc7ed2c4f8b8a76fae6a6bdebc1e7d7c7c71c2
VictorMartosSegura <victormartossegura@elpuig.xeill.net>
2026-05-04 02:01:27 +0200

    Add role queries to user repository

--- src/main/java/com/autovalor/user/UserRepository.java
+++ src/main/java/com/autovalor/user/UserRepository.java
@@ -1,5 +1,6 @@
 package com.autovalor.user;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -7,4 +8,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
+
+    boolean existsByRole(Role role);
+
+    List<User> findAllByRole(Role role);
 }
