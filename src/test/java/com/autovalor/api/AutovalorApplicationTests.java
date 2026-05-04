57b6d5906c6e47d76ab55b1023c6ab118eac5c02
VictorMartosSegura <victormartossegura@elpuig.xeill.net>
2026-05-04 02:40:57 +0200

    Use H2 configuration in context load test

--- src/test/java/com/autovalor/api/AutovalorApplicationTests.java
+++ src/test/java/com/autovalor/api/AutovalorApplicationTests.java
@@ -2,12 +2,26 @@ package com.autovalor.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:autovalor_context_test;MODE=PostgreSQL;DATABASE_TO_LOWER=TRUE;DEFAULT_NULL_ORDERING=HIGH",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.jpa.open-in-view=false",
+        "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect",
+        "app.jwt.secret=autovalor-context-test-jwt-secret-with-more-than-32-characters",
+        "app.jwt.expiration-minutes=1440",
+        "app.admin.name=AutoValor Admin",
+        "app.admin.email=admin-context@autovalor.test",
+        "app.admin.password=AdminContext123"
+})
 class AutovalorApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
