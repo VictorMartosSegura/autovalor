7055be09fd6ef38fe3bdadaeed425a157d06f19e
VictorMartosSegura <victormartossegura@elpuig.xeill.net>
2026-05-15 18:22:43 +0200

    Use Spring Framework MockitoBean import

--- src/test/java/com/autovalor/api/ListingImageIntegrationTest.java
+++ src/test/java/com/autovalor/api/ListingImageIntegrationTest.java
@@ -22,10 +22,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockitoBean;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
